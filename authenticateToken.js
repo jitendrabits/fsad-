55f7d6f3b3ac6ce04745c941abbe2b051e5922e4
jitendrabits <161793270+jitendrabits@users.noreply.github.com>
2024-05-21 21:42:09 +0530

    Create authenticateToken.js

---
+++ authenticateToken.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const secretKey = process.env.SECRET_KEY || "elearnwilpbitspilani";
+
+
+// Middleware to authenticate JWT token
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = authenticateToken;
